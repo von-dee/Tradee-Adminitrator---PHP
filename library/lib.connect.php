9d8bce8a3a860c47eeea77e6e0689fd806708fee
Sela <vondeesela@gmail.com>
2021-09-20 20:19:40 +0000

    lib.connect

---
+++ library/lib.connect.php
@@ -0,0 +1,21 @@
+<?php
+/**
+###################################################
+### This file contains database server settings ###
+###################################################
+ **/
+
+$engine	  = "mysqli";
+$port     = "3306";
+
+// $server   = "localhost";
+// $username = "root";
+// $password = ""; 
+
+$server   = "157.230.183.69";
+$username = "root";
+$password = "tiynkpassword"; 
+
+$database = "tradee";
+
+?>
